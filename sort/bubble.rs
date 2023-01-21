44162ed920390e4bed264a0601abc9b2c731cabf
Kaneki Ken <toranjain07@gmail.com>
2023-01-21 23:41:00 +0530

    implemented bubble sort in bubble.rs and updated cargo.toml for its binary

---
+++ sort/bubble.rs
@@ -0,0 +1,15 @@
+fn bubble<T: std::cmp::PartialEq + std::cmp::PartialOrd>(arr: &mut[T]) {
+	for i in 0..arr.len() {
+		for j in 0..arr.len()-1-i {
+			if arr[j] > arr[j+1]{
+				arr.swap(j, j+1);
+			}
+		}
+	}
+}
+
+fn main() {
+	let mut arr = [1,4,3,2,7,5,6];
+	bubble(&mut arr);
+	println!("{:?}", arr);
+}
