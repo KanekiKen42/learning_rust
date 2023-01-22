cd71078ed1d6f98c0f769508e92187349004ac68
Kaneki Ken <toranjain07@gmail.com>
2023-01-22 22:31:14 +0530

    fixed both iterative and recursive binary searches

--- search/binary.rs
+++ search/binary.rs
@@ -1,6 +1,6 @@
 fn rec_binary<T: std::cmp::PartialEq + std::cmp::PartialOrd>(arr: &[T], target: T, left: usize, right: usize) -> i32{
 	let mid = (left+right)/2;
-	if right > left {
+	if right >= left {
 		if arr[mid] == target {
 			return mid as i32
 		} else if arr[mid] < target {
@@ -18,23 +18,24 @@ fn itr_binary<T: std::cmp::PartialEq + std::cmp::PartialOrd>(arr: &[T], target: 
 	let mut mid: usize = 0;
 	loop {
 		mid = (left+right)/2;
-		if right > left {
+		if right >= left {
 			if arr[mid] == target {
 				return mid as i32
 			} else if arr[mid] > target {
-				right = mid-1;
+				right = mid - 1 ;
 			} else if arr[mid] < target {
-				left = mid+1;
-			}
+				left = mid + 1;
+			} 
+		} else {
+			break;
 		}
-		break;
 	}
 	return -1
 }
 
 fn main(){
-	let arr = [1,2,3,4,5,6,7,8,9,10];
-	let target = 5;
+	let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+	let target = 9;
 	let index = rec_binary(&arr, target, 0, arr.len()-1);
 	match index {
 		-1 => println!("Element Not Found"),
